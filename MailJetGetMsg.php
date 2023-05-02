c89ce4d3e21d5f2a3f3813dc18ed24838ed2e569
Girish Shenoy <girish@vishwayon.com>
2023-05-02 15:58:16 +0530

    keys-removed

--- MailJetGetMsg.php
+++ MailJetGetMsg.php
@@ -3,6 +3,6 @@
 require 'vendor/autoload.php';
 use \Mailjet\Resources;
 
-$mj = new \Mailjet\Client('c53d5c922db9927320da27d762afbca2', '0ffd80670fd2bf1ed7c89a76b6c27feb');
-$response = $mj->get(Resources::$Message, ['id' => '576460768532263887']);
+$mj = new \Mailjet\Client('key', 'secret');
+$response = $mj->get(Resources::$Message, ['id' => 'mail-id']);
 var_dump($response->getData());
